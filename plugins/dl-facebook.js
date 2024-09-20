f33579f40f9f2ba9d2eb12f561850627d8caf23d
Opaksfire <opakstg@gmail.com>
2024-09-20 18:52:11 +0100

    © opaksfire

---
+++ plugins/dl-facebook.js
@@ -0,0 +1,42 @@
+import fetch from "node-fetch"
+
+const handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) {
+    throw `✳️ Please send the link of a Facebook video\n\n📌 EXAMPLE :\n*${usedPrefix + command}* https://fb.watch/tXadtHWTjf/?mibextid=lbJOhI7Z2ZfpRMIi`
+  }
+
+  const urlRegex =
+    /^(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i
+  if (!urlRegex.test(args[0])) {
+    throw '⚠️ PLEASE GIVE A VALID URL.'
+  }
+
+  let url = `https://api.guruapi.tech/fbvideo?url=${args[0]}`
+
+  m.react(🔥)
+
+  try {
+    const result = await fetch(url)
+    const tex = `
+⊱ ─── {* FIRE MD 🔥*} ─── ⊰
+↳ *VIDEO TITLE:* ${result.result.title}
+⊱ ────── {⋆♬⋆} ────── ⊰`
+
+    const response = await fetch(result.result.hd)
+    const arrayBuffer = await response.arrayBuffer()
+    const videoBuffer = Buffer.from(arrayBuffer)
+
+    conn.sendFile(m.chat, videoBuffer, 'fb.mp4', tex, m)
+    m.react(done)
+  } catch (error) {
+    console.log(error)
+    m.reply('⚠️ An error occurred while processing the request. Please try again later.')
+  }
+}
+
+handler.help = ['facebook <url>']
+handler.tags = ['downloader']
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i
+handler.diamond = true
+
+export default handler
---
+++ plugins/firemd-dark.js
@@ -0,0 +1,38 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  try {
+    if (!text) throw 'uhm.. what do you want to say?';
+    await m.react('🤖');
+
+    const prompt = encodeURIComponent(text);
+    let apiurl = `https://dark.guruapi.tech/egpt?prompt=${prompt}`;
+
+    const result = await fetch(apiurl);
+    const response = await result.json();
+    
+    if (!response.message) throw 'No result found';
+
+    const replyText = response.message;
+    await conn.sendButton(
+      m.chat, 
+      replyText, 
+      author, 
+      'https://api.shannmoderz.xyz/server/file/XyjKP6IA0VnyFZF.jpg', 
+      [['Script', `.sc`]], 
+      null, 
+      [['Follow Me', `https://github.com/`]], 
+      m
+    );
+  } catch (error) {
+    console.error(error);
+    m.reply('Oops! Something went wrong. We are trying hard to fix it ASAP.');
+  }
+};
+
+handler.help = ['darky <text>'];
+handler.tags = ['tools'];
+handler.command = ['darky', 'darkgpt']
+
+export default handler;
+
