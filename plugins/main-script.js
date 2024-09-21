9b9803fed14f1f4c955ab4f7d46a309e317b8ffd
Opaksfire <opakstg@gmail.com>
2024-09-21 21:01:42 +0100

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import { join } from 'path'
 import axios from 'axios'
 
 let handler = async function (m, { conn, __dirname }) {
-  const githubRepoURL = 'https://github.c'
+  const githubRepoURL = 'https://github.com/Opaksfire/FIRE-MD'
 
   try {
     const [, username, repoName] = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/)
