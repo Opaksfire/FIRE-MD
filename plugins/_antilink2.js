fde131be312e386d238146f45498857d48bb0209
Opaksfire <opakstg@gmail.com>
2024-10-09 23:23:45 +0100

    Add files via upload

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -19,15 +19,15 @@ if (m.text.includes(linkThisGroup)) return !0
 if (m.text.includes(linkThisGroup2)) return !0
 if (m.text.includes(linkThisGroup3)) return !0
 }    
-await this.sendMessage(m.chat, { text: `https://github.com/Opaksfire
+await this.sendMessage(m.chat, { text: `No links here 🔥
 *「 𝐀𝐍𝐓𝐈 𝐋𝐈𝐍𝐊𝐒 」*\n*Sorry 𝙱𝙰𝙱𝚈 👋 ${user} YOU BROKE THE GROUP RULES, YOU WILL BE EXTAMINATED...!!*`, mentions: [m.sender] })  
-if (!isBotAdmin) return m.reply('https://github.com/Opaksfire/FIRE-MD *[❗𝐈𝐍𝐅𝐎❗]  THE BOT IS NOT ADMIN, IT CANNOT SCREEN PEOPLE*')
+if (!isBotAdmin) return m.reply('Make me an admin *[❗𝐈𝐍𝐅𝐎❗]  THE BOT IS NOT ADMIN, IT CANNOT SCREEN PEOPLE*')
 //await conn.sendButton(m.chat, `*「 𝐀𝐍𝐓𝐈 𝐋𝐈𝐍𝐊𝐒 」*\n*𝙷𝙰𝚂𝚃𝙰 𝙻𝙰 𝚅𝙸𝚂𝚃𝙰 𝙱𝙰𝙱𝚈 👋, ${await this.getName(m.sender)} 𝚁𝙾𝙼𝙿𝙸𝚂𝚃𝙴𝚂 𝙻𝙰𝚂 𝚁𝙴𝙶𝙻𝙰𝚂 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙴𝚁𝙰𝚂 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙳𝙾...!!*${isBotAdmin ? '' : '\n\n*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽, 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝚁 𝙰 𝙻𝙰𝚂 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝚂*'}`, author, ['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺𝚂', '/disable antilink'], m)    
 if (isBotAdmin && bot.restrict) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return
-} else if (!bot.restrict) return m.reply('https://github.com/Opaksfire/FIRE-MD *[❗𝐈𝐍𝐅𝐎❗]The owner of the bot does not have the restrictions enabled (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) contact him to enable it*')
+} else if (!bot.restrict) return m.reply(' *[❗𝐈𝐍𝐅𝐎❗]The owner of the bot does not have the restrictions enabled (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) contact him to enable it*')
 }
 return !0
 }
